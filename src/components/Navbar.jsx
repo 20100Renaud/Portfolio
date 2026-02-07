4c95f27eb1ad91d1487996ea3b3d5e5ccee4dd9e
Vincent-12156 <12156@holbertonstudents.com>
2026-02-07 20:16:37 +0100

    Create App with layout

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,66 @@
+import { Link } from "react-router-dom"
+
+export default function Navbar() {
+  return (
+    <div className="navbar bg-base-100 shadow-sm px-4">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+          </ul>
+        </div>
+
+        <Link to="/" className="btn btn-ghost text-xl ml-2">
+          Vincent
+        </Link>
+      </div>
+
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className="navbar-end">
+        <Link to="/contact" className="btn">
+          Contact
+        </Link>
+      </div>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// @ts-nocheck
+import daisyui from "daisyui"
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  plugins: [daisyui],
+  daisyui: { themes: ["light"] },
+}
