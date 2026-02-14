ea331ca3e61be2e4ac488035ff37db74cd383f27
Vincent-12156 <12156@holbertonstudents.com>
2026-02-14 19:28:45 +0100

    Force light theme

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ const shareItems = [
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-grow items-center text-center w-full relative text-black">
+    <div className="flex flex-col flex-grow items-center text-center w-full relative">
         <h1 className="text-2xl sm:text-3xl font-bold">
             Share what others want !
         </h1>
