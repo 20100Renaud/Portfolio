8656f155524f4a71df837d0e5eb23146a9b80a06
Vincent-12156 <12156@holbertonstudents.com>
2026-02-15 09:44:37 +0100

    Fix icons paths

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -100,7 +100,7 @@ export default function Navbar() {
           <img
             src={logo}
             alt="logo"
-            className="h-10 w-auto transition-transform duration-200 hover:scale-110"
+            className="h-14 w-auto "
           />
         </Link>
 
