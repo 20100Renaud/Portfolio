1053e700a6bdb4f7f3bc33438731736c408728e2
Vincent-12156 <12156@holbertonstudents.com>
2026-03-07 19:47:38 +0100

    Fix url

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,6 +24,6 @@ app.get("/test", async (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on ${PORT}`)
 })
