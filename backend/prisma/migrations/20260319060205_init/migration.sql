ae8b9fe55a16d077eb54d378d9f61fd7b331b537
Vincent-12156 <12156@holbertonstudents.com>
2026-03-19 07:15:24 +0100

    Update db structure

---
+++ backend/prisma/migrations/20260319060205_init/migration.sql
@@ -0,0 +1,48 @@
+-- CreateTable
+CREATE TABLE "T_Clients" (
+    "ID_Client" TEXT NOT NULL,
+    "Login_Client" TEXT NOT NULL,
+    "Mail_Client" TEXT NOT NULL,
+    "Password_Client" TEXT NOT NULL,
+    "PC_Client" TEXT NOT NULL,
+    "Date_Client" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "T_Clients_pkey" PRIMARY KEY ("ID_Client")
+);
+
+-- CreateTable
+CREATE TABLE "T_Posts" (
+    "ID_Post" TEXT NOT NULL,
+    "Title_Post" TEXT NOT NULL,
+    "Description_Post" TEXT NOT NULL,
+    "Date_Post" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "ID_Client_Post" TEXT NOT NULL,
+
+    CONSTRAINT "T_Posts_pkey" PRIMARY KEY ("ID_Post")
+);
+
+-- CreateTable
+CREATE TABLE "T_Comments" (
+    "ID_Com" TEXT NOT NULL,
+    "Description_Com" TEXT NOT NULL,
+    "Date_Com" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "ID_Post_Com" TEXT NOT NULL,
+    "ID_Client_Com" TEXT NOT NULL,
+
+    CONSTRAINT "T_Comments_pkey" PRIMARY KEY ("ID_Com")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "T_Clients_Login_Client_key" ON "T_Clients"("Login_Client");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "T_Clients_Mail_Client_key" ON "T_Clients"("Mail_Client");
+
+-- AddForeignKey
+ALTER TABLE "T_Posts" ADD CONSTRAINT "T_Posts_ID_Client_Post_fkey" FOREIGN KEY ("ID_Client_Post") REFERENCES "T_Clients"("ID_Client") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "T_Comments" ADD CONSTRAINT "T_Comments_ID_Post_Com_fkey" FOREIGN KEY ("ID_Post_Com") REFERENCES "T_Posts"("ID_Post") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "T_Comments" ADD CONSTRAINT "T_Comments_ID_Client_Com_fkey" FOREIGN KEY ("ID_Client_Com") REFERENCES "T_Clients"("ID_Client") ON DELETE RESTRICT ON UPDATE CASCADE;
