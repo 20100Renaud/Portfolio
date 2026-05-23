6b24196d77ad3a3718f4c653dcf9652cdad04d41
EnzoMessaoudi <12168@holbertonstudents.com>
2026-05-23 07:54:44 +0200

    Fix the white page problem when the user created an account

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -35,7 +35,7 @@ export default function Dashboard() {
 
   return (
     <div className="relative text-center text-green-900 overflow-hidden justify-center my-10 mx-auto">
-      <h1 className="text-3xl sm:text-5xl font-bold">Welcome {username} </h1>
+      <h1 className="text-3xl sm:text-5xl font-bold">Welcome {username.username} </h1>
       <p className="text-sm sm:text-lg text-green-800">
         Here we can find our field.
         <button
