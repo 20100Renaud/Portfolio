10210059252e81f0c2a2a32ae4b4597842038e38
EnzoMessaoudi <12168@holbertonstudents.com>
2026-06-10 10:40:38 +0200

    Change schema of the database

---
+++ backend/prisma/migrations/20260610081615_remove/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `PC_Client` on the `T_Clients` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "T_Clients" DROP COLUMN "PC_Client";
