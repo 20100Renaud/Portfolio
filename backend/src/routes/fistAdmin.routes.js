be73e31eb537941ab59abb59d70ceeb08dfe73a4
EnzoMessaoudi <12168@holbertonstudents.com>
2026-06-12 06:48:00 +0200

    Remove old admin creation

--- backend/src/routes/fistAdmin.routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { createUser } from "../controllers/clients.controller.js";
-import { firstAdminOnly } from "../middleware/firstAdmin.middleware.js";
-
-const router = express.Router();
-
-router.post("/first-admin", firstAdminOnly, createUser);
