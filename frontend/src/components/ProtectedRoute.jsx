dabdc742908f663121a15e908f6bd6f1fb1170b9
EnzoMessaoudi <12168@holbertonstudents.com>
2026-06-12 07:30:31 +0200

    ADD a loading so that the app wait for the server response before doing anything

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -2,7 +2,11 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/useAuth";
 
 export default function ProtectedRoute({ children }) {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
