9eb077e6f8947448a8134c159b9d3e63327d4924
EnzoMessaoudi <12168@holbertonstudents.com>
2026-06-12 06:48:56 +0200

    Put the username in first place

--- backend/src/validators/auth.schema.js
+++ backend/src/validators/auth.schema.js
@@ -2,9 +2,9 @@ import { z } from "zod"
 
 
 export const registerSchema = z.object({
+  username: z.string().min(4),
   email: z.email(),
   password: z.string().min(4),
-  username: z.string().min(4),
   ville_client:z.string(),
   latitude_client:z.number(),
   longitude_client:z.number()
