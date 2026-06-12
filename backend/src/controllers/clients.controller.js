38b7f548f384098b931eed12f3fbb4061aa1f0ad
EnzoMessaoudi <12168@holbertonstudents.com>
2026-06-12 08:16:05 +0200

    Change the post to ad

--- backend/src/controllers/clients.controller.js
+++ backend/src/controllers/clients.controller.js
@@ -27,7 +27,7 @@ export const deleteClient = async (req, res) => {
     });
 
     await prisma.T_Comments.deleteMany({
-      where: { ID_Post_Com: post.ID_Post }
+      where: { ID_Ads_Com: ad.ID_Ad }
     });
 
     await prisma.T_Clients.delete({ where: { ID_Client: clientId } });
