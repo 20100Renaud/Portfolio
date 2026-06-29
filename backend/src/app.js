c5278ef9c19af3955331de15c702c577b32d28b0
EnzoMessaoudi <12168@holbertonstudents.com>
2026-06-29 13:01:51 +0200

    Merge dev-Enzo with dev-Vincent

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,27 @@
-// app.js
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
+import cookieParser from "cookie-parser";
+
+import prisma from "./prismaClient.js";
+
 import authRoutes from "./routes/auth.routes.js";
 import deposRoutes from "./routes/depos.routes.js";
-import prisma from "./prismaClient.js";
-import cookieParser from "cookie-parser";
+import citiesRoutes from "./routes/cities.routes.js";
+
+import { startDepoCleanupJob } from "./jobs/cleanupDepos.job.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
+startDepoCleanupJob();
+
+// --------------- Global middleware ---------------
 app.use((req, res, next) => {
   if (req.url !== "/health") {
     console.log(`[REQ] ${req.method} ${req.url}`);
@@ -24,16 +33,21 @@ app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
-  })
+  }),
 );
-
 app.use(express.json());
 app.use(cookieParser());
-app.use("/uploads", express.static("uploads"));
 
+// --------------- Static files ---------------
+app.use("/uploads", express.static("uploads"));
+app.use(express.static(path.join(__dirname, "public")));
+
+// --------------- API routes ---------------
 app.use("/api/auth", authRoutes);
 app.use("/api/depos", deposRoutes);
+app.use("/api/cities", citiesRoutes);
 
+// --------------- Specific endpoints ---------------
 app.get("/api/test", async (req, res) => {
   try {
     const users = await prisma.T_Users.findMany();
@@ -48,8 +62,7 @@ app.get("/health", (req, res) => {
   res.json({ ok: true });
 });
 
-app.use(express.static(path.join(__dirname, "public")));
-
+// --------------- Frontend fallback ---------------
 app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
