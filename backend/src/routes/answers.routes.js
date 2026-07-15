2c9471f18247a4f117f50360405a67200cc3236d
EnzoMessaoudi <12168@holbertonstudents.com>
2026-07-15 15:27:30 +0200

    FIX the calling route

--- backend/src/routes/answers.routes.js
+++ backend/src/routes/answers.routes.js
@@ -9,6 +9,8 @@ import prisma from "../prismaClient.js";import {
     updateAnswer,
     deleteAnswer,
 } from "../controllers/answers.controller.js";
+import {UpdateLimiter} from "../middleware/answer.middleware.js";
+import { CreateLimiter } from "../middleware/depo.middleware.js";
 
 const router = express.Router();
 
